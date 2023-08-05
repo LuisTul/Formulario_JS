02c29e1678f099e94ceb6ae3e76839d2d9144441
LuisTul <luis360123@gmail.com>
2023-08-05 12:20:31 -0600

    Se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Header from "./components/Header/Header";
+import Formulario from './components/Formulario/Formulario';
+import MiOrg from './components/MiOrg';
 
 function App() {
+  const [mostrarFormulario,actualizarMostrar] = useState(true)
+  //Ternario --> condición ? seMuestra : noSemuestra
+  //Condición && seMuestra
+  const cambiarMostrar = () =>{
+    actualizarMostrar(!mostrarFormulario)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {/* {mostrarFormulario ? <Formulario /> : <></>} */}     
+      {mostrarFormulario && <Formulario />}
+      <MiOrg cambiarMostrar={cambiarMostrar}/>
     </div>
   );
 }
